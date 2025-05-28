491391a54c80f5bce3516e44f9865fb81a5db375
MusicBox <hillarybakasa9@gmail.com>
2025-05-28 21:42:23 +0200

    my changes

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -271,14 +271,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "c2c335fa7bb30d171ca36b3a697ec134
 "cors.json": "6b990e0f8ebd7f32b86dcb21ca38a114",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "383e55f7f3cce5be08fcf1f3881f585c",
-"flutter_bootstrap.js": "226ae7ba0c1e702dd55fa08146a4b1cb",
+"flutter_bootstrap.js": "2ed758ba2ecba7d110edc4ec15c04a14",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "a6d3458117cd54bd9fa4630bd5427e6d",
 "/": "a6d3458117cd54bd9fa4630bd5427e6d",
-"main.dart.js": "b93fb76ab7f365922cbbac29e5e36707",
+"main.dart.js": "ba250000c60c3c89bbffc55dd156541b",
 "manifest.json": "857d7438891fe081a903fd431d44ce05",
 "version.json": "868c6fca6cfa40393c07f0ab75b73b60"};
 // The application shell files that are downloaded before a service worker can
