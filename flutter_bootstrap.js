e8449eebfda5a078a0ecd785d03c89b574f51a18
MusicBox <hillarybakasa9@gmail.com>
2025-05-30 22:22:17 +0200

    my changes

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"235db911ba279722f5e685f38b0ed30fa7e857
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3747232797"
+    serviceWorkerVersion: "2810956783"
   }
 });
